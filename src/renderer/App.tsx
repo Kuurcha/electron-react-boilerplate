a923712193133559c9d7d5aad8781c7ebc6fd78a
Kurcha <theyhyrik@gmail.com>
2025-10-07 19:53:49 +0300

    Обновление разметки

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -8,20 +8,55 @@ function Hello() {
       <div className="Hello">
         <img width="200" alt="icon" src={icon} />
       </div>
-      <h1>electron-react-boilerplate</h1>
+
+      <div className="relative max-w-sm">
+        <div className="flex justify-center">
+          <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+            <svg
+              className="w-28 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fillRule="evenodd"
+                d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          <input
+            // eslint-disable-next-line react/no-unknown-property
+            id="default-datepicker"
+            type="number"
+            min="1"
+            onInput={(e: any) => {
+              if (e.target.value < 1) e.target.value = '1';
+            }}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+             focus:ring-blue-500 focus:border-blue-500
+             ps-5 p-1.5
+             dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
+             dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Укажите количество секунд"
+          />
+        </div>
+      </div>
+
       <div className="Hello">
         <button
           type="button"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
         >
-          Default
+          Начать захват
         </button>
 
         <button
           type="button"
           className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
         >
-          Red
+          Остановить захват
         </button>
       </div>
     </div>
